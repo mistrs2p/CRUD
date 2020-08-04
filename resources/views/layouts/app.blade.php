fecd274992aa2e6687924f4591758c68b88f4227
MistRain@P <mahdimousavi40@gmail.com>
2020-08-04 22:17:45 +0430

    New branch added - fonts added - some scss files created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        {{-- Public style css --}}
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
         <title>{{ config('app.name', 'CrudApp') }}</title>
 
         <!-- Fonts -->

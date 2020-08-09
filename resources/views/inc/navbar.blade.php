b7180af6a643e92247d89015fc5be80cf66ab8cf
MistRain@P <mahdimousavi40@gmail.com>
2020-08-10 01:50:14 +0430

    layout.blade.php and navbar.blade.php fixed with older changes

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,40 +1,70 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-  <a class="navbar-brand" href="/">{{ config('app.name', 'CrudApp') }}</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
+  <div class="container">
+      <a class="navbar-brand" href="{{ url('/') }}">
+          {{ config('app.name', 'CRUDapp') }}
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+          <span class="navbar-toggler-icon"></span>
+      </button>
 
-  <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="/">خانه <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/about">درباره ما</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/services" tabindex="-1" aria-disabled="true">خدمات ما</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/posts" tabindex="-1" aria-disabled="true">وبلاگ</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-        <div class="dropdown-menu" aria-labelledby="dropdown01">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-    </ul>
-    <ul class="nav navbar-nav justify-content-center">
-      <li class="nav-item"><a href="/posts/create" class="nav-link">ایجاد پست</a></li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <div class="form-group">
-        <input class="form-control ml-sm-2" type="text" placeholder="جست و جوی موضوع مورد نظر ..." aria-label="Search">
-        <button class="btn btn-secondary my-2 my-sm-0" type="submit">جست و جو</button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/">خانه <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">درباره ما</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/services" tabindex="-1" aria-disabled="true">خدمات ما</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/posts" tabindex="-1" aria-disabled="true">وبلاگ</a>
+          </li>
+          
+        </ul>
+          <!-- Left Side Of Navbar -->
+          <ul class="navbar-nav mr-auto">
+
+          </ul>
+          <ul class="nav navbar-nav justify-content-center">
+            <li class="nav-item"><a href="/posts/create" class="nav-link">ایجاد پست</a></li>
+          </ul>
+
+          
+          <ul class="navbar-nav">
+              <!-- Authentication Links -->
+              @guest
+                  <li class="nav-item">
+                      <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                  </li>
+                  @if (Route::has('register'))
+                      <li class="nav-item">
+                          <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                      </li>
+                  @endif
+              @else
+                  <li class="nav-item dropdown">
+                      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                          {{ Auth::user()->name }} <span class="caret"></span>
+                      </a>
+
+                      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                          <a class="dropdown-item" href="{{ route('logout') }}"
+                             onclick="event.preventDefault();
+                                           document.getElementById('logout-form').submit();">
+                              {{ __('Logout') }}
+                          </a>
+
+                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                              @csrf
+                          </form>
+                      </div>
+                  </li>
+              @endguest
+          </ul>
       </div>
-    </form>
   </div>
 </nav>

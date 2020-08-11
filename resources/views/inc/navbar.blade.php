4ad55bc896feb81763b314fbcd31e81b8b06b903
MistRain@P <mahdimousavi40@gmail.com>
2020-08-12 02:13:29 +0430

    PostsController@store user_id & navbar dropdown dashboard link added

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -50,6 +50,9 @@
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('dashboard') }}">
+                            {{ __('داشبورد') }}
+                        </a>
                         <a class="dropdown-item" href="{{ route('logout') }}"
                             onclick="event.preventDefault();
                                           document.getElementById('logout-form').submit();">

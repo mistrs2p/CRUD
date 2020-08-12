c48e984fce3c21f0fb61f2832bbab44247cab47b
MistRain@P <mahdimousavi40@gmail.com>
2020-08-12 17:41:25 +0430

    input file added to create view with enctype

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>ایجاد پست</h1>
-    <form action="{{ action('PostsController@store') }}" method="POST">
+    <form action="{{ action('PostsController@store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <div class="form-group">
             <label for="title">عنوان</label>
@@ -13,7 +13,9 @@
             <label for="body">متن بدنه</label>
             <textarea class="form-control" id="editor1" name="body" placeholder="متن بدنه" style="font-family: 'IRANSansFaNum'"></textarea>
         </div>
-
+        <div class="form-group">
+            <input type="file" name="cover_image">
+        </div>
         <button type="submit" class="btn btn-primary">ایجاد پست</button>
     </form>
     

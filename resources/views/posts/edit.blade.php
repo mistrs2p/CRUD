973c50ea56137b14a9e8e6aff5638290f845d1d1
MistRain@P <mahdimousavi40@gmail.com>
2020-08-12 20:29:58 +0430

    cover_image input field adde edit view

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h1>ویرایش پست</h1>
-    <form action="{{ action('PostsController@update', ['post' => $post]) }}" method="POST">
+    <form action="{{ action('PostsController@update', ['post' => $post]) }}" method="POST" enctype="multipart/form-data">
         <input type="hidden" name="_method" value="put" />
         @csrf
         <div class="form-group">
@@ -15,6 +15,10 @@
             <textarea class="form-control" id="editor1" name="body" placeholder="متن بدنه" style="font-family: 'IRANSansFaNum'">
                 {{$post->body}}
             </textarea>
+        </div>
+
+        <div class="form-group">
+            <input type="file" name="cover_image" value="">
         </div>
 
         <button type="submit" class="btn btn-primary">ویرایش پست</button>

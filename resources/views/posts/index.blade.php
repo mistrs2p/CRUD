f2537a6260a605bbd7ba278b88a4067cbe1fd436
MistRain@P <mahdimousavi40@gmail.com>
2020-08-12 20:25:14 +0430

    cover_image in index view added

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,11 +6,16 @@
     @if (count($posts) > 0)
         @foreach ($posts as $post)
             <div class="card mb-3">
-                <div class="card-body">
-                    <div class="card-title">
-                        <h3><a href="/posts/{{$post->id}}">{{ $post->title }}</a></h3>
+                <div class="card-body row">
+                    <div class="col-md-4 col-sm-4">
+                        <img src="/storage/cover_images/{{ $post->cover_image }}" width="100%">
                     </div>
-                   <small>نوشته شده در {{ $post->created_at }} توسط {{ $post->user->name }}</small>
+                    <div class="col-md-8 col-sm-8">
+                        <div class="card-title">
+                            <h3><a href="/posts/{{$post->id}}">{{ $post->title }}</a></h3>
+                        </div>
+                       <small>نوشته شده در {{ $post->created_at }} توسط {{ $post->user->name }}</small>
+                    </div>
                 </div>
             </div>
         @endforeach
